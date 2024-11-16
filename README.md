5322d937959356ae84f9cb0bb2ede50cf0fbbb73
rizh <92094675+heyrzx@users.noreply.github.com>
2024-11-16 18:47:09 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# auto-request-approve-bot-
+Auto-Request-Approve-bot
+
+## 🏷 Environment Variables #
+  - `API_ID` - Your Telegram API ID.Get it [Here](my.telegram.org)
+  - `API_HASH` - Your Telegram API HASH.Get it [Here](my.telegram.org)
+  - `DB_URL` - Add MongoDB Database URI.
+  - `DB_NAME` - Add Mongodb Database Name.
+  - `BOT_USERNAME` - Add Bot Username.
+  - `BOT_TOKEN` - Your Bot Token. Get it from [Here](https://t.me/BotFather)
+  - `FORCE_SUB` - Your Force subscribe channel id Get it from @MissRose_Bot
+  - `LOG_CHANNEL` - Bot Logs Sending Channel. If You Don't Need This To Remove This Variable In Your Server
+  - `ADMIN` - bot owners Id/ ids ( for broadcast and stats cmds). for multiple use space.
+
+  Available Commond:-
+  ```
+start - Check Bot Alive.
+stats - Check Bot Status.
+broadcast - Broadcast Massage Send All Users In Bot.
+restart - Send Message All Users In Bot & Bot Restart & Re-Deploy Server.
+```
+
---
+++ configs.py
@@ -0,0 +1,37 @@
+from os import path, getenv
+import os, time 
+
+class Config:
+    API_ID = int(getenv("API_ID", "15043670"))
+    API_HASH = getenv("API_HASH", "d131b85fafc3e98cde4c506cccee3f2a")
+    BOT_TOKEN = getenv("BOT_TOKEN", "5866366012:AAFbsYOno-w3KCU0TR_-HTcs7-_3sRbl5cE")
+ 
+    FORCE_SUB = int(getenv("FORCE_SUB", "-1001823248949"))
+    ADMIN = list(map(int, getenv("ADMIN", "6051042088").split()))
+    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001930341741"))
+    
+    # database configs
+    DB_URL = os.environ.get("DB_URL", "mongodb+srv://dudemusic111:dudemusic111@cluster0.df3yis2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+    DB_NAME = os.environ.get("DB_NAME", "Cluster1")
+    
+    #web response 
+    WEBHOOK = bool(os.environ.get("WEBHOOK", True))
+    BOT_UPTIME  = time.time()
+    PORT = os.environ.get("PORT", "8080")
+    
+    RKN_PIC = os.environ.get("RKN_PIC", "https://envs.sh/Lss.jpg")
+    
+ # Subsprice Gif & Auto Request Accept
+    SURPRICE = os.environ.get("SURPRICE", "https://graph.org/file/28c188cb93dc4c08132f9.mp4").split()
+
+    LOGO = """
+RZXBOTS"""
+
+rkn1 = Config()
+
+
+# Rkn Developer 
+# Don't Remove Credit 😔
+# Telegram Channel @RknDeveloper & @Rkn_Botz
+# Developer @RknDeveloperr
+# Update Channel @Digital_Botz & @DigitalBotz_Support
